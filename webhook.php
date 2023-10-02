38fcab952b5150c89533e903d084954e01355f03
jesusroap <mauricio.roa@outlook.com>
2023-10-01 23:10:54 -0500

    actualizacion de webhook

--- webhook.php
+++ webhook.php
@@ -13,9 +13,36 @@ if ($hubSignature != "sha1=" . hash_hmac("sha1", file_get_contents("php://input"
     die("Firma no válida.");
 }
 
-// Actualizar el repositorio y construir la aplicación
-exec("git pull origin master");
-exec("ng build --prod");
+// Ruta del directorio de origen
+$directorioOrigen = '/dist';
+
+// Ruta del directorio de destino
+$directorioDestino = '../';
+
+// Abre el directorio de origen
+if ($gestorDirectorio = opendir($directorioOrigen)) {
+    // Recorre cada archivo en el directorio de origen
+    while (false !== ($nombreArchivo = readdir($gestorDirectorio))) {
+        // Ignora los directorios "." y ".."
+        if ($nombreArchivo != "." && $nombreArchivo != "..") {
+            // Crea la ruta completa de origen y destino para el archivo
+            $rutaOrigen = $directorioOrigen . $nombreArchivo;
+            $rutaDestino = $directorioDestino . $nombreArchivo;
+
+            // Mueve el archivo a la nueva ubicación
+            if (rename($rutaOrigen, $rutaDestino)) {
+                echo "El archivo '$nombreArchivo' se ha movido correctamente.<br>";
+            } else {
+                echo "Error al mover el archivo '$nombreArchivo'.<br>";
+            }
+        }
+    }
+
+    // Cierra el directorio de origen
+    closedir($gestorDirectorio);
+} else {
+    echo "No se pudo abrir el directorio de origen '$directorioOrigen'.<br>";
+}
 
 // Puedes agregar más acciones, como notificar por correo electrónico o reiniciar servicios si es necesario.
 
